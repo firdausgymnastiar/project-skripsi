b9cbdcaf4e4ba677c9765a792750bac7ad793e66
firdaus <firdaus.gymnastiar@gmail.com>
2024-05-29 22:31:00 +0700

    home page

--- web.py
+++ web.py
@@ -285,4 +285,4 @@ def generatetoken():
         return jsonify({'success': False, 'message': str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
